289614fd856d9bfea778496b662a668d08c51503
Ihor Podvalniy <bembasua@gmail.com>
2022-10-03 17:36:43 +0300

    Чек

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,35 @@
+import { Box } from './Box/Box';
+import shortid from 'shortid';
 import { Component } from 'react';
+import { Form } from './Form/Form';
+
 export class App extends Component {
   state = {
-    contacts: [],
-    name: '',
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+  };
+
+  handleSubmit = name => {
+    const contact = {
+      name,
+    };
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+    console.log(this.state.contacts);
   };
   render() {
     return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        React homework template
-      </div>
+      <Box>
+        <Form onSubmit={this.handleSubmit}></Form>
+        {this.state.contacts.map(contact => {
+          return <div key={shortid.generate()}>{contact.name}</div>;
+        })}
+      </Box>
     );
   }
 }
